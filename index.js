e7760b30e5ada3e7a825ca65f70ffe37d70bec69
manishcs50class <pokharakomanish@gmail.com>
2023-02-07 13:00:50 -0500

    Created a 16 x 16 grid

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const cont = document.getElementById("container");
-const
-for (var i = 1; i <)
+const container = document.getElementById("container");
 
+function createGrid(rows, cols) {
+    // for (let i = 0; i < 16; i++) {
+    //     for (let j = 0; j < 16; j++) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
-
-
-
+    // create rows * cols number of cells inside container div
+    for (cellCount = 0; cellCount < (rows * cols); cellCount++) {
+        let cell = document.createElement("div");
+        cell.innerText = (cellCount + 1);
+        container.appendChild(cell).className = "grid";
+    };
+};
+createGrid(16, 16);
