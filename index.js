37d888cace25ae7bed0b9d0452fc17411475e529
manishcs50class <pokharakomanish@gmail.com>
2023-02-07 14:24:25 -0500

    Make changes to the grid so as to individually interpret each cells

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const container = document.getElementById("container");
 
 function createGrid(rows, cols) {
-    // for (let i = 0; i < 16; i++) {
-    //     for (let j = 0; j < 16; j++) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
     // create rows * cols number of cells inside container div
     for (cellCount = 0; cellCount < (rows * cols); cellCount++) {
         let cell = document.createElement("div");
-        cell.innerText = (cellCount + 1);
+        // Cells are unique because we have to change color individually
+        cell.setAttribute("id", cellCount + 1);
+
+        // Make cells visible.
+        cell.style.border = "0.1em solid black";
+        cell.style.background = "white";
+        cell.style.padding = "1em"
+
         container.appendChild(cell).className = "grid";
+
+
     };
 };
 createGrid(16, 16);
