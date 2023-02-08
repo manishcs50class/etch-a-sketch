ad77d4309f51ad2e68ed89e70a32396d72a171ef
manishcs50class <pokharakomanish@gmail.com>
2023-02-08 15:41:05 -0500

    On mouseover the grid cells change color

--- index.js
+++ index.js
@@ -3,43 +3,47 @@ const container = document.getElementById("container");
 //Getting user input for number of rows
 let inputRows;
 function getInputFromUser() {
-    inputRows = prompt("Enter No. of Rows");
-    container.replaceChildren();
+  inputRows = prompt("Enter No. of Rows");
+  container.replaceChildren();
 
-    createGrid(inputRows, inputRows);
+  createGrid(inputRows, inputRows);
 }
 
 //resetting the grid
 function resetGrid() {
-    container.replaceChildren();
-    createGrid(inputRows, inputRows);
+  container.replaceChildren();
+  createGrid(inputRows, inputRows);
 }
 
-function addColor(e) {
-    e.target.style = 'background-color':
-    rgb(${ Math.floor(Math.random) })
+function colorGradient() {}
+
+function getRandomRGBColor() {
+  var red = Math.floor(Math.random() * 256); // range is 0-255
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
 }
 
 function createGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
 
-    // create rows * cols number of cells inside container div
-    for (cellCount = 0; cellCount < (rows * cols); cellCount++) {
-        let cell = document.createElement("div");
-        // Cells are unique because we have to change color individually
-        cell.setAttribute("id", cellCount + 1);
+  // create rows * cols number of cells inside container div
+  for (cellCount = 0; cellCount < rows * cols; cellCount++) {
+    let cell = document.createElement("div");
+    // Cells are unique because we have to change color individually
+    cell.setAttribute("id", cellCount + 1);
 
-        // Make cells visible.
-        cell.style.border = "0.1em solid black";
-        cell.style.background = "white";
-        cell.style.padding = "1em"
+    // Make cells visible.
+    cell.style.border = "0.1em solid black";
+    cell.style.background = "white";
+    cell.style.padding = "1em";
 
-        container.appendChild(cell).className = "grid";
+    container.appendChild(cell).className = "grid";
 
-        // change the color of individual cells to black.
-        container.addEventListener("mouseover", (event) => {
-            event.target.style.background = 'black';
-        });
-    };
-};
+    // change the color of individual cells to black.
+    container.addEventListener("mouseover", (event) => {
+      event.target.style.background = getRandomRGBColor();
+    });
+  }
+}
